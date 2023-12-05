65be449e10288acd2e00eb6943c15a24793e097f
Cristian-Matei.Toader <cristian-matei.toader@lseg.com>
2023-12-05 18:04:20 +0200

    c1: finished exercises

--- exercises/src/test/java/c1_Introduction.java
+++ exercises/src/test/java/c1_Introduction.java
@@ -2,11 +2,13 @@ import org.junit.jupiter.api.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,9 +42,7 @@ public class c1_Introduction extends IntroductionBase {
     @Test
     public void hello_world() {
         Mono<String> serviceResult = hello_world_service();
-
-        String result = null; //todo: change this line only
-
+        String result = serviceResult.block();
         assertEquals("Hello World!", result);
     }
 
@@ -54,8 +54,7 @@ public class c1_Introduction extends IntroductionBase {
     public void unresponsive_service() {
         Exception exception = assertThrows(IllegalStateException.class, () -> {
             Mono<String> serviceResult = unresponsiveService();
-
-            String result = null; //todo: change this line only
+            String result = serviceResult.block(Duration.ofSeconds(1));
         });
 
         String expectedMessage = "Timeout on blocking read for 1";
@@ -72,7 +71,7 @@ public class c1_Introduction extends IntroductionBase {
     public void empty_service() {
         Mono<String> serviceResult = emptyService();
 
-        Optional<String> optionalServiceResult = null; //todo: change this line only
+        Optional<String> optionalServiceResult = serviceResult.blockOptional(); //todo: change this line only
 
         assertTrue(optionalServiceResult.isEmpty());
         assertTrue(emptyServiceIsCalled.get());
@@ -89,7 +88,8 @@ public class c1_Introduction extends IntroductionBase {
     public void multi_result_service() {
         Flux<String> serviceResult = multiResultService();
 
-        String result = serviceResult.toString(); //todo: change this line only
+        String result = serviceResult.take(1)
+                                     .blockFirst();
 
         assertEquals("valid result", result);
     }
@@ -103,7 +103,9 @@ public class c1_Introduction extends IntroductionBase {
     public void fortune_top_five() {
         Flux<String> serviceResult = fortuneTop5();
 
-        List<String> results = emptyList(); //todo: change this line only
+        List<String> results = serviceResult.take(5)
+                                            .toStream()
+                                            .collect(Collectors.toList());
 
         assertEquals(Arrays.asList("Walmart", "Amazon", "Apple", "CVS Health", "UnitedHealth Group"), results);
         assertTrue(fortuneTop5ServiceIsCalled.get());
@@ -126,12 +128,13 @@ public class c1_Introduction extends IntroductionBase {
 
         Flux<String> serviceResult = fortuneTop5();
 
-        serviceResult
-                .doOnNext(companyList::add)
-        //todo: add an operator here, don't use any blocking operator!
-        ;
+        serviceResult.doOnNext(companyList::add)
+                     .take(5)
+                     .subscribe();
 
-        Thread.sleep(1000); //bonus: can you explain why this line is needed?
+        //bonus: can you explain why this line is needed?
+        // a: main thread calls are async and may equals assertion may happen ahead of list being populated
+        Thread.sleep(1000);
 
         assertEquals(Arrays.asList("Walmart", "Amazon", "Apple", "CVS Health", "UnitedHealth Group"), companyList);
     }
@@ -152,8 +155,8 @@ public class c1_Introduction extends IntroductionBase {
         CopyOnWriteArrayList<String> companyList = new CopyOnWriteArrayList<>();
 
         fortuneTop5()
-        //todo: change this line only
-        ;
+                .take(5)
+                .subscribe(companyList::add, ex -> {}, () -> serviceCallCompleted.set(true));
 
         Thread.sleep(1000);
 
